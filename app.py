ed2b02fce7cb7f0f5ff8af8192d04659c450fca2
Diego <A01227808@itesm.mx>
2019-04-08 16:00:12 -0500

    added endpoints

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,6 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import Book
+from models import Usuarios
 
 @app.route("/")
 def hello():
@@ -44,6 +46,79 @@ def get_by_id(id_):
     try:
         book=Book.query.filter_by(id=id_).first()
         return jsonify(book.serialize())
+    except Exception as e:
+	    return(str(e))
+
+
+@app.route("/register")
+def register():
+    nombre=request.args.get('nombre')
+    apellido=request.args.get('apellido')
+    email=request.args.get('email')
+    password=request.args.get('password')
+    edad=request.args.get('edad')
+    ciudad=request.args.get('ciudad')
+    genero=request.args.get('genero')
+    estado=request.args.get('estado')
+    interes=request.args.get('interes')
+    fotoPerfil=request.args.get('fotoPerfil')
+    try:
+        usuario=Usuarios(
+            nombre=nombre,
+            apellido=apellido,
+            email=email,
+            password=password,
+            edad=edad,
+            ciudad=ciudad,
+            genero=genero,
+            estado=estado,
+            interes=interes,
+            fotoPerfil=fotoPerfil
+        )   
+        db.session.add(usuario)
+        db.session.commit()
+        return "User added. user id={}".format(usuario.id)
+    except Exception as e:
+	    return(str(e))
+
+@app.route("/getUsers")
+def get_users():
+    try:
+        usuarios=Usuarios.query.all()
+        return  jsonify([e.serialize() for e in usuarios])
+    except Exception as e:
+	    return(str(e))
+
+@app.route("/login")
+def login():
+    email=request.args.get('email')
+    password=request.args.get('password')
+    try:
+        usuario = Usuarios.query.filter_by(password=password, email=email).first()
+        if(usuario==None):
+            dic={"success":"False", "userId":-1}
+            return json.dumps(dic)
+        dic={"success":"True", "userId":usuario.id}
+        return json.dumps(dic)
+    except Exception as e:
+	    return(str(e))
+
+@app.route("/getProfile")
+def getProfile():
+    userId=request.args.get('userID')
+    try:
+        usuario = Usuarios.query.filter_by(id=userId).first()
+        name= usuario.nombre
+        gender=usuario.genero
+        age=usuario.edad
+        userState=usuario.estado
+        image=usuario.fotoPerfil
+        lastName=usuario.apellido
+        if usuario==None:
+            dic={"success":"False", "name":None,"lastName":None, "gender":None, "age":None, "userState":None, "image":None}
+            return json.dumps(dic)
+        dic={"success":"True", "name":name, "lastName":lastName, "gender":gender, "age":age, "userState":userState, "image":image}
+        return json.dumps(dic)
     except Exception as e:
 	    return(str(e))
 
--- models.py
+++ models.py
@@ -23,3 +23,55 @@ class Book(db.Model):
             'author': self.author,
             'published':self.published
         }
+
+class Usuarios(db.Model):
+    __tablename__ = 'usuarios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String())
+    apellido= db.Column(db.String())
+    email = db.Column(db.String())
+    password = db.Column(db.String())
+    edad = db.Column(db.String())
+    ciudad = db.Column(db.String())
+    genero = db.Column(db.String())
+    estado = db.Column(db.String())
+    interes = db.Column(db.String())
+    fotoPerfil = db.Column(db.String())
+
+    def __init__(self, nombre, apellido, email, password, edad, ciudad, genero, estado, interes, fotoPerfil):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.email=email
+        self.password=password
+        self.edad=edad
+        self.ciudad=ciudad
+        self.genero=genero
+        self.estado=estado
+        self.interes=interes
+        self.fotoPerfil=fotoPerfil
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    def serialize(self):
+        return {
+            'id': self.id,
+            'nombre': self.nombre,
+            'apellido':self.apellido,
+            'email':self.email,
+            'edad':self.edad,
+            'genero':self.genero,
+            'estado':self.estado,
+            'interes':self.interes,
+            'urlPerfil': self.fotoPerfil
+        }
+
+
+"""
+//perfil
+nombre
+imagen
+genero
+estado
+"""
